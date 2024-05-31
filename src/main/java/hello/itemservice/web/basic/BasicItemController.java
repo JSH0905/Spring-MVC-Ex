0e3fbe0630820fa70d79ef9e08fa4ab21685d9e8
JSH0905 <2000tjdgns@naver.com>
2024-05-31 15:43:20 +0900

    수정 사항 처리 완료 / 상품 수정 처리 컨트롤러, 뷰 구현

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -97,8 +97,28 @@ public class BasicItemController {
         itemRepository.save(item);
 //        model.addAttribute("item", item); // 자동 추가, 생략 가능
 
-        return "/basic/item";
+        return "basic/item";
     }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+
+        itemRepository.update(itemId, item);
+
+        return "redirect:/basic/items/{itemId}";
+    }
+
+
+
 
     /**
      * 테스트용 데이터 추가
